42c68a3ed14e92e14a34169ac2e9da04de85397c
Daniel870608 <aibity870608@gmail.com>
2019-08-23 15:49:54 +0800

    123

---
+++ MVCex/MVCex/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using MVCex.Models;
+using MVCex.Services;
+using MVCex.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCex.Controllers
+{
+    public class HomeController : Controller
+    {
+        public GuestbookService guestbookService = new GuestbookService();
+        public ActionResult Index(string Search = "", int page = 1)
+        {
+            Guestbook IndexViewModel = new Guestbook();
+            IndexViewModel.ForPaging = new ForPaging(page);
+            IndexViewModel.DataList = guestbookService.GetAllGuestbooks(IndexViewModel.ForPaging, Search);
+            return View(IndexViewModel);
+        }
+
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Add(Gbook Data)
+        {
+            guestbookService.InsertNewGuestbook(Data);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            Gbook Data = guestbookService.GetDataById(Id);
+            return View(Data);
+        }
+        [HttpPost]
+        public ActionResult Edit(Gbook EditData)
+        {
+            guestbookService.EditGuestbook(EditData);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Reply(int Id)
+        {
+            Gbook Data = guestbookService.GetDataById(Id);
+            return View(Data);
+        }
+        [HttpPost]
+        public ActionResult Reply(Gbook ReplyData)
+        {
+            guestbookService.ReplyGuestbook(ReplyData);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            guestbookService.DeleteGuestbook(Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
